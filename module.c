c17ee34d4315c8d265181336527614592b006035
Hubert Mantel <mantel@novell.com>
2000-02-01 15:01:17 +0000

    Copyright 2000

--- module.c
+++ module.c
@@ -2,7 +2,7 @@
  *
  * module.c      Load modules needed for installation
  *
- * Copyright (c) 1996-1999  Hubert Mantel, SuSE GmbH  (mantel@suse.de)
+ * Copyright (c) 1996-2000  Hubert Mantel, SuSE GmbH  (mantel@suse.de)
  *
  */
 
