10c8525c981a72b21293834cb3fe24b4cf6db8d9
Steffen Winterfeldt <wfeldt@opensuse.org>
2001-08-06 16:48:34 +0000

    - fixed for alpha

--- settings.c
+++ settings.c
@@ -40,7 +40,8 @@ typedef struct
       char *mapname;
       } keymap_t;
 
-#if defined(__i386__) || defined(__PPC__) || defined(__ia64__) || defined(__s390__) || defined(__sparc__)
+#if 1	/* I think the line below would catch all anyway... */
+// #if defined(__i386__) || defined(__PPC__) || defined(__ia64__) || defined(__s390__) || defined(__sparc__) || defined(__alpha__)
 
 #define LANG_DEFAULT	1
 static language_t set_languages_arm [] =
