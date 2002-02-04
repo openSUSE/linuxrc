84d1ec8234b47f1c952d6ae26f975063162e4e61
Hubert Mantel <mantel@novell.com>
2002-02-04 11:36:26 +0000

    Fixed Copyright entries (SuSE Linux AG)

--- multiple_info.h
+++ multiple_info.h
@@ -2,7 +2,7 @@
  *
  * multiple_info.h        Header file for multiple_info.c
  *
- * Copyright (c) 2000-2001  Matthias Eckermann, SuSE (mge@suse.de)
+ * Copyright (c) 2000-2002  Matthias Eckermann, SuSE Linux AG  (mge@suse.de)
  *
  */
 
