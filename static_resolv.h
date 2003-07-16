ee03ff1ba83b4c556e5ce5bf403c179fb6b7acb8
Steffen Winterfeldt <wfeldt@opensuse.org>
2003-07-16 10:44:57 +0000

    *** empty log message ***

---
+++ static_resolv.h
@@ -0,0 +1,9 @@
+#if !defined(DIET) && !defined(UC)
+
+struct hostent *res_gethostbyaddr(const char *addr, int len, int type);
+struct hostent *res_gethostbyname(const char *name);
+
+#define gethostbyaddr res_gethostbyaddr
+#define gethostbyname res_gethostbyname
+
+#endif
