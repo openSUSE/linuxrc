2a25f449fb64582947e8126877628e5ec17e0e35
Steffen Winterfeldt <wfeldt@opensuse.org>
2004-07-05 14:14:12 +0000

    This commit was manufactured by cvs2svn to create tag 'linuxrc-1_7_0'.

--- insmod/include/elf_alpha.h
+++
@@ -1,9 +0,0 @@
-/* Machine-specific elf macros for the Alpha.  */
-
-#define ELFCLASSM	ELFCLASS64
-#define ELFDATAM	ELFDATA2LSB
-
-#define MATCH_MACHINE(x)  (x == EM_ALPHA)
-
-#define SHT_RELM	SHT_RELA
-#define Elf64_RelM	Elf64_Rela
--- insmod/include/elf_sparc.h
+++
@@ -1,9 +0,0 @@
-/* Machine-specific elf macros for the Sparc.  */
-
-#define ELFCLASSM	ELFCLASS32
-#define ELFDATAM	ELFDATA2MSB
-
-#define MATCH_MACHINE(x)  (x == EM_SPARC)
-
-#define SHT_RELM	SHT_RELA
-#define Elf32_RelM	Elf32_Rela
