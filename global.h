594d9b0fbe3f1d5eaac705f0cafcf85f4d4ead0b
aginies <aginies@suse.com>
2013-10-09 11:34:30 +0200

    new full automatic installation method using download.opensuse.org mirror (http) and auto configuration of the path using the product number.

--- global.h
+++ global.h
@@ -125,7 +125,7 @@ typedef struct {
  */
 typedef enum {
   inst_none = 0, inst_file, inst_nfs, inst_ftp, inst_smb,
-  inst_http, inst_https, inst_tftp, inst_cdrom, inst_floppy, inst_hd,
+  inst_http, inst_auto, inst_https, inst_tftp, inst_cdrom, inst_floppy, inst_hd,
   inst_dvd, inst_cdwithnet, inst_net, inst_slp, inst_exec,
   inst_rel, inst_disk
 } instmode_t;
--- url.c
+++ url.c
@@ -559,6 +559,7 @@ url_t *url_set(char *str)
     url->scheme == inst_ftp ||
     url->scheme == inst_smb ||
     url->scheme == inst_http ||
+    url->scheme == inst_auto ||
     url->scheme == inst_https ||
     url->scheme == inst_tftp
     ) {
@@ -759,6 +760,7 @@ char *url_print_zypp(url_t *url)
     scheme != inst_file &&
     scheme != inst_ftp &&
     scheme != inst_http &&
+    scheme != inst_auto &&
     scheme != inst_https &&
     scheme != inst_nfs &&
     scheme != inst_smb &&
@@ -1226,6 +1228,7 @@ int url_mount_disk(url_t *url, char *dir, int (*test_func)(url_t *))
         break;
 
       case inst_http:
+      case inst_auto:
       case inst_https:
       case inst_ftp:
       case inst_tftp:
@@ -2807,3 +2810,17 @@ int digest_verify(url_data_t *url_data, char *file_name)
   return ok;
 }
 
+char *get_product_version()
+{
+  /* parse config.product to find the name and the version */
+   char *product_info; char *product_version;
+   int count = 0;
+   product_info = strtok(config.product, " ");
+   for (count= 0; count < 2 ; count++)
+   {
+      if (count == 1) product_version = product_info;
+      product_info = strtok(NULL, " ");
+   }
+   if(config.debug) { fprintf(stderr, "product version: %s\n", product_version); }
+   return product_version;
+}
