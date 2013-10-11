62bd1ece77e1f4b3112ab3b5bb1ec2ba4a6dfa84
aginies <aginies@suse.com>
2013-10-11 12:57:55 +0200

    update the date

--- linuxrc.c
+++ linuxrc.c
@@ -784,7 +784,7 @@ void lxrc_init()
     if (config.linemode)
       putchar('\n');
     printf(
-      "\n>>> %s installation program v" LXRC_FULL_VERSION " (c) 1996-2012 SUSE Linux Products GmbH <<<\n",
+      "\n>>> %s installation program v" LXRC_FULL_VERSION " (c) 1996-2013 SUSE Linux Products GmbH <<<\n",
       config.product
     );
     if (config.linemode)
