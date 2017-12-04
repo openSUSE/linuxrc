2ede25e49184857e4cc307040045d0e44b2f376b
Steffen Winterfeldt <wfeldt@opensuse.org>
2017-12-04 13:32:09 +0100

    adjust date

--- linuxrc.c
+++ linuxrc.c
@@ -894,7 +894,7 @@ void lxrc_init()
     if (config.linemode)
       putchar('\n');
     printf(
-      "\n>>> %s installation program v" LXRC_FULL_VERSION " (c) 1996-2016 SUSE LLC <<<\n",
+      "\n>>> %s installation program v" LXRC_FULL_VERSION " (c) 1996-2017 SUSE LLC <<<\n",
       config.product
     );
     if (config.linemode)
